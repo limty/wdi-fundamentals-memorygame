ede8af5c44a5cc7b3949e806230a0124b478a081
LTY <limtheanyew@gmail.com>
2019-05-03 22:36:46 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,5 @@
-console.log("Up and running!");
+/*console.log("Up and running!");
 
-/*
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
@@ -10,9 +9,22 @@ var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
 var cardOne = cards[0];
+var cardTwo = cards[2];
+
 cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-console.log("User flipped queen");
+if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+}
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+console.log("User flipped "+cardOne);
+console.log("User flipped "+cardTwo);
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
