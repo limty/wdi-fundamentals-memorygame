7439272d961e5d697fcea481cfb9283e1f4624d1
LTY <limtheanyew@gmail.com>
2019-05-03 22:25:13 +0800

    added cards and cardsInPlay array

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,18 @@
 console.log("Up and running!");
 
+/*
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
+*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped queen");
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
