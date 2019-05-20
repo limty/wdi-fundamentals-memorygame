3f2ef3b278af9a01d9d412160eec510048d7ff99
LTY <limtheanyew@gmail.com>
2019-05-20 16:30:10 +0800

    created game logic still have errors

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,23 +8,33 @@ var cardFour = "king";
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+function checkForMatch() {
+	//if(cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
+	//}
+};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function flipCard(cardId) {
+	checkForMatch();
+	/*
+	var cardOne = cards[0];
+	var cardTwo = cards[2];
 
-if(cardsInPlay.length === 2){
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}
-	else{
-		alert("Sorry, try again.");
-	}
-}
+	cardsInPlay.push(cardOne);
+	cardsInPlay.push(cardTwo);
+	*/
+	console.log("User flipped "+ cards[cardId]);
+	
+	/*
+	console.log("User flipped "+cardOne);
+	console.log("User flipped "+cardTwo);
+	*/
+	cardsInPlay.push(cards[cardId]);
+};
 
-console.log("User flipped "+cardOne);
-console.log("User flipped "+cardTwo);
-
-//console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardTwo);
+flipCard(0);
+flipCard(2);
