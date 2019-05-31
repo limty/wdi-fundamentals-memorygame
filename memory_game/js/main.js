9db26ad5b9ce2309bacd73aefc6485a5b9ca2b33
LTY <limtheanyew@gmail.com>
2019-05-31 22:26:20 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,5 @@
-/*console.log("Up and running!");
+var score = 0;
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-*/
-//var cards = ["queen", "queen", "king", "king"];
 var cards = [
 {
 	rank: "queen",
@@ -31,35 +25,53 @@ var cards = [
 
 var cardsInPlay = [];
 
+function updateScore(inputScore){
+	document.getElementById('score-text').textContent = inputScore;
+};
+
+function resetGame(){
+	/*
+	//Doesn't work
+	document.getElementById('cardElement').setAttribute('src', "images/back.png");//"flip" cards back into back images
+	score = 0;
+	updateScore(score);
+	*/
+	location.reload(); //reloads page
+}
+
 function checkForMatch() {
 	if (cardsInPlay.length > 1){
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+		if (cardsInPlay[-1] === cardsInPlay[-2]) {
+			window.alert("You found a match!");
 			console.log("You found a match!");
+			score++; //add 1 to score
+			updateScore(score); //update score
 		} else {
-			//console.log("Sorry, try again.");
 			window.alert("Sorry, try again.");
+			console.log("Sorry, try again.");
 		}
 	}
 };
 
-function flipCard(cardId) {
-	/*
-	var cardOne = cards[0];
-	var cardTwo = cards[2];
-
-	cardsInPlay.push(cardOne);
-	cardsInPlay.push(cardTwo);
-	*/
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
-	/*
-	console.log("User flipped "+cardOne);
-	console.log("User flipped "+cardTwo);
-	*/
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 };
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+    	var cardElement = document.createElement('img');
+    	cardElement.setAttribute('src', "images/back.png");
+    	cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener('click',flipCard); // No '' in function parameters!
+    	document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
+document.getElementById("button-reset").addEventListener('click',resetGame);
